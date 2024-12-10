fdaa94b0c8816d1d81e1c881b66a501fcacdadf9
Rajat Tripathi <rivalo3.chat@gmail.com>
2024-12-10 14:49:29 +0530

    Completed 01-js-easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
-
+if(str1.length != str2.length){
+  return false;
 }
-
+str1 = str1.toLowerCase();
+str2 = str2.toLowerCase();
+let string_1 = str1.split('').sort().join('');
+let string_2 = str2.split('').sort().join('');
+for (let i=0 ; i<str1.length; i++){
+  if(string_1[i] != string_2[i]){
+    return false;
+  }
+}
+return true;
+}
 module.exports = isAnagram;
