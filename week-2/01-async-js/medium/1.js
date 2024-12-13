eec1f98d9a569982124270a092e4ad98545c56f7
Rajat Tripathi <rivalo3.chat@gmail.com>
2024-12-13 19:00:55 +0530

    Minor changes.

--- week-2/01-async-js/medium/1.js
+++ week-2/01-async-js/medium/1.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 let statement = "";
 fs.readFile("text_file.txt", "utf8" , function(err,data){
     statement = data;
-    // statement = statement.replace(/[^a-zA-Z0-9\s]/g,''); // Used to remove any extra non-alphanumeric characters from the string.
+    // statement = statement.replace(/[^a-zA-Z0-9\s]/g,''); 
+    // Used to remove any extra non-alphanumeric characters from the string.
     statement = statement.replace(/\s+/g, ' ');
     console.log(statement.trim());
 });
