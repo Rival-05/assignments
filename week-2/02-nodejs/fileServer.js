7ccda8d66f5e0f23a83d3ef55322bd25fd3c462f
Rival-05 <rivalo3.chat@gmail.com>
2025-02-04 22:49:00 +0530

    Added solution to Local_File_Fetcher.

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,38 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+const { error } = require('console');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const filesDir = path.join(__dirname, './files/');
 
+app.get('/files',(req,res) =>{
+  fs.readdir(filesDir,(err,files) =>{
+    if(err){
+      return res.status(500).json({
+        error: 'Failes to retrieve files.'
+      })
+    }
+    res.status(200).json(files);
+  });
+})
 
+app.get('/file/:filename',(req,res) =>{
+  const filePath = path.join(filesDir, req.params.filename);
+  fs.readFile(filePath, 'utf8' , (err,data) =>{
+    if(err){
+      return res.status(404).send('File not found');
+    }
+    else{
+      res.status(200).send(data); 
+    }
+  })
+})
+
+app.use((req,res) =>{
+  res.status(404).send('Route not found');
+})
+// app.listen(3000);
 module.exports = app;
