3e081735a879498f1fb7701c2aed29c13cccc0fd
Rajat Tripathi <rivalo3.chat@gmail.com>
2025-06-27 23:39:55 +0530

    Done with week-03 jwt

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod = require("zod");
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,8 +13,18 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }  
+    const token = jwt.sign({username},jwtPassword);
+    return token;
 }
 
 /**
@@ -26,7 +36,13 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    try{
+        jwt.verify(token,jwtPassword); 
+    }
+    catch{
+        return false;
+    }
+    return true;
 }
 
 /**
@@ -37,7 +53,13 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const decoded = jwt.decode(token); 
+    if(decoded){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
