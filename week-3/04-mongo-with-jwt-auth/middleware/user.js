23e367d1390498ca63727a65645f20adc93716a1
Rajat Tripathi <rivalo3.chat@gmail.com>
2025-06-29 18:04:41 +0530

    nothing special

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,21 @@
+const jwt = require("jsonwebtoken");
+const {jwtsecret} = require("../config");
 function userMiddleware(req, res, next) {
     // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    // You need to check the headers and validate the user from the user DB. Check readme for 
+    // the exact headers to be expected
+    const token = req.headers.authorization;
+    const realToken = token.split(" ");
+    const jwtToken = realToken[1];
+        
+    const verified = jwt.verify(jwtToken,jwtsecret);
+    if(verified.username){
+        next();
+    }else{
+    res.status(403).json({
+        msg:"Username or Password invalid."
+        })
+    }
 }
 
 module.exports = userMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,46 @@
+const jwt = require("jsonwebtoken");
+const {jwtSecret} = require("../config")
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        msg:"Admin created successfully."
+    })
+
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post('/signin', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const response = await Admin.findOne({
+        username : username,
+        password : password
+    })
+    
+    if(response){
+        const signedtoken = jwt.sign({username}, jwtSecret);
+        res.json({
+            signedtoken
+        })
+    }
+    else{
+        res.status(411).json({
+            msg: "Incorrect email or password."
+        })
+    }
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
